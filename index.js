27a93aceb3b52b40f3f1a804bf0afdb9c107b2e7
Vyacheslav Botalov <Vyacheslav.Botalov@gmail.com>
2016-03-20 00:26:09 +0500

    File sync read was used.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
+var fs = require('fs');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var testContent = fs.readFileSync('index.html').toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(testContent)
 })
 
 app.listen(app.get('port'), function() {
